f017f97b687666525ea9122b13b2369135e5dc71
Roopin <bhadriroopin748@gmail.com>
2020-04-11 06:50:16 +0530

    Updates

---
+++ precache-manifest.9702bc35fc22574c046ce6e92dba5090.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80330ea0a0b75f681bcb1090b96c694d",
+    "url": "/Dad-Jokes/index.html"
+  },
+  {
+    "revision": "2f0a0923c832676c4405",
+    "url": "/Dad-Jokes/static/css/main.2e48f5be.chunk.css"
+  },
+  {
+    "revision": "7e9a50d4280deda3e032",
+    "url": "/Dad-Jokes/static/js/2.38b7c724.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Dad-Jokes/static/js/2.38b7c724.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f0a0923c832676c4405",
+    "url": "/Dad-Jokes/static/js/main.807d25b9.chunk.js"
+  },
+  {
+    "revision": "7877d265729a9be73e08",
+    "url": "/Dad-Jokes/static/js/runtime-main.7997e475.js"
+  }
+]);
